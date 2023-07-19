1de274c179d0f425e39fdbba0335f33f0ca88d42
Dev Mukherjee <devraj@gmail.com>
2023-07-19 12:12:57 +1000

    chore: port helm chart configuration

--- Terraform/modules/helm/helm.tf
+++ Terraform/modules/helm/helm.tf
@@ -28,3 +28,48 @@ resource "helm_release" "aws-load-balancer-controller" {
         var.eks_cluster
     ]
 }
+
+
+# resource "helm_release" "lab-python-mock-server" {
+
+#   name       = "lab-python-mock-server"
+#   repository = "oci://ghcr.io/anomaly/charts"
+#   chart      = "lab-python-mock-server"
+#   namespace  = "app-backend"
+
+#   version = "0.3.0"
+
+#   create_namespace = true
+
+#   set {
+#     name  = "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/group\\.name"
+#     value = "python-app-ingress"
+#     type  = "string"
+#   }
+
+#   depends_on = [
+#     helm_release.aws-load-balancer-controller,
+#   ]
+
+# }
+
+
+# resource "helm_release" "remix-aws-template" {
+
+#   name             = "remix-aws-template"
+#   repository       = "oci://ghcr.io/anomaly/charts"
+#   chart            = "remix-aws-template"
+#   namespace        = "app-frontend"
+#   create_namespace = true
+
+#   set {
+#     name  = "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/group\\.name"
+#     value = "python-app-ingress"
+#     type  = "string"
+#   }
+
+#   depends_on = [
+#     helm_release.aws-load-balancer-controller,
+#   ]
+
+# }
