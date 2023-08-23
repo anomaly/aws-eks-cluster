d8d969e16ee05579885c5d4eea4324520604184e
Dev Mukherjee <devraj@gmail.com>
2023-08-23 10:11:05 +1000

    feat: port ses configuration

--- Terraform/modules/sns/output.tf
+++ Terraform/modules/sns/output.tf
@@ -3,6 +3,7 @@
 output "topics" {
     value = {
         auto_scaling_notifications = aws_sns_topic.auto_scaling_notifications
-        redis_notifications = aws_sns_topic.redis_notifications
+        redis_notifications = aws_sns_topic.redis_notifications,
+        ses_notifications = aws_sns_topic.ses_notifications
     }
 }
